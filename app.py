ad5f11a286e239098f8a18addc7e09d2c07f383d
Sarah Sohail <ssohail.bee22seecs@seecs.edu.pk>
2025-12-17 00:34:53 +0500

    Modifying app.py again again for listening to dynamic port.

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def upload_file():
 
 if __name__ == '__main__':
     # Ensure to listen on the dynamic port provided by Railway
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
 
 
